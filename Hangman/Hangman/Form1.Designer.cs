326b03f2e91ca1c7f879bbd5e265b16b7cf0f3f6
tomti <tomti@DESKTOP-M45J832>
2016-06-17 19:34:14 +0100

    Enter key submits presses button and can't resize window.

--- Hangman/Hangman/Form1.Designer.cs
+++ Hangman/Hangman/Form1.Designer.cs
@@ -61,14 +61,16 @@
             // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(229, 259);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Hangman";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
